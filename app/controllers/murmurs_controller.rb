55c2906911c6ccdfa666e5b7fceb411f2bd2ea64
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-04-09 13:57:27 +0900

    compiled about murmur

---
+++ app/controllers/murmurs_controller.rb
@@ -0,0 +1,58 @@
+class MurmursController < ApplicationController
+  before_action :set_murmur, only: [:edit,:destroy,:update,:show]
+
+  def index
+    @murmur = Murmur.all
+  end
+
+  def new
+    @murmur = Murmur.new
+  end
+
+  def create
+    @murmur = Murmur.new(maurmur_params)
+    if @murmur.save
+      redirect_to murmur_path, notice: "Murmured！"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+
+  end
+
+  def confirm
+    @murmur = Murmur.new(tubuyaki_params)
+  end
+
+  def edit
+    @murmur = Murmur.find(params[:id])
+  end
+
+  def update
+    if @murmur.update(tubuyaki_params)
+      redirect_to tubuyakis_path, notice: "Modified murmur！"
+    else
+      render 'edit'
+    end
+  end
+
+
+  def destroy
+    @tubuyaki = Tubuyaki.find(params[:id])
+    @tubuyaki.destroy
+    redirect_to murmur_path, notice: "Delited murmur"
+  end
+
+  private
+
+  def murmur_params
+    params.require(:tubuyaki).permit(:content)
+  end
+
+  def set_murmur
+    @murmur = Murmur.find(params[:id])
+  end
+
+end
